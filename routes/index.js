198f405361358fbe51a1213f8cb68433f06be426
gato365 <immanuelwilliams@gato365.com5>
2022-12-21 09:59:38 -0800

    Made a JSON file for notes and fsUtils to read in info

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,8 @@ const router = require('express').Router();
 // 2. Import our modular routers 
 const notesRouter = require('./notes');
 
-
 // 4.
 app.use('/notes', notesRouter);
 
- 
 // 5.
 module.exports = router;
