9d505b1b318f76da51060ed877fc347b4fbae827
gato365 <immanuelwilliams@gato365.com5>
2022-12-22 07:26:19 -0800

    Added comments to server

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// 1. 
+// 1. Load Libraries
 const express = require('express');
 const path = require('path');
 
-// 2.
-// const { clog } = require('./middleware/clog');
+// 2. Load Local modules
+const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
 
-// 3. 
+// 3. Load Port
 const PORT = process.env.PORT || 3001;
 
-// 4. 
+// 4. Load app
 const app = express();
 
 // 5. Import custom middleware, "cLog"
@@ -21,10 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
-// 7. 
+// 7. Gain access to Frontend (HTML material)
 app.use(express.static('public'));
 
-//8.
+//8. Get pages
 
 // 8a. GET Route for homepage
 app.get('/', (req, res) =>
@@ -36,15 +36,12 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
-
-
-// 8d. GET Wildcard route to direct users to a 404 page
+// 8c. GET Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/404.html'))
 );
 
-// 9.
+// 9. Create Listener
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
