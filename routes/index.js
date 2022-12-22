5a5e58887b6b050c7b3f2d4b0de56cf4cd01c6e9
gato365 <immanuelwilliams@gato365.com5>
2022-12-22 07:36:49 -0800

    Added comments to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-// 1.
+// 1. Load Libraries
 const router = require('express').Router();
 
 // 2. Import our modular routers 
 const notesRouter = require('./notes');
 
-// 4.
-app.use('/notes', notesRouter);
+// 3. Use router and notes 
+router.use('/notes', notesRouter);
 
-// 5.
+// 4. Export module
 module.exports = router;
